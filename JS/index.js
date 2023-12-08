f9c724ce255444d416d85b822d5ed25becb9286d
Leamourad <139025477+Leamourad@users.noreply.github.com>
2023-12-07 18:47:30 -0600

    new version
    added folders

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+/*
+const button01 = document.querySelector("button01")
+button01.addEventListener ("mouseover"), () => {
+button01.style.bordercolor= "1px solid black"; 
+};
+   
+button01.addEventListener ("mouseout"), () => {
+
+
+box.style.border = "#8C9CB6";
+button01.style.bordercolor= "1px solid red";
+}
+button01.addEventListener ("mouseover"); 
+button01.addEventListener ("mouseout");
+*/
